4ead9de0c2eed14ec787b023bcf30abc3ffe13e5
delikeyki1016 <149133856+delikeyki1016@users.noreply.github.com>
2024-07-09 22:21:30 +0900

    7/10 과제 커밋

--- 0708_news/news.js
+++ 0708_news/news.js
@@ -10,10 +10,11 @@
 // pageSize : 기사갯수
 // page : 기사갯수가 10개면 10개의 기사가 있는 1page
 
+// 누나 API : https://beautiful-torte-d702c9.netlify.app/top-headlines?country=kr&apiKey=
 // https://newsapi.org/v2/top-headlines?country=kr&apiKey=70a9dc1efaba49299c95e70ba34ae4ab&pageSize=1
 const API_KEY = `70a9dc1efaba49299c95e70ba34ae4ab`;
 
-let news = [];
+let newsList = [];
 
 const getNews = async () => {
     const url = new URL(
@@ -25,8 +26,36 @@ const getNews = async () => {
     console.log("rsponse:", response);
     const data = await response.json(); // json파일형태로 data변수에 선언
     console.log("data:", data);
-    news = data.articles;
-    console.log("news:", news);
+    newsList = data.articles;
+    console.log("newsList:", newsList);
+    render();
+};
+
+const render = () => {
+    let newsHTML = ``;
+
+    newsHTML = newsList
+        .map((item) => {
+            return `<div class="row p-2 border-bottom">
+                <div class="col-lg-4">
+                ${
+                    item.urlToImage && item.urlToImage
+                        ? `<img src=${item.urlToImage} alt="" class="photo-size"  />`
+                        : `<i class="fa-regular fa-image photo-size"></i>`
+                }
+                </div>
+                <div class="col-lg-8 d-flex flex-column">
+                    <h2>${item.title}</h2>
+                    <p>${item.description}</p>
+                    <div class="mt-auto">${item.author} | ${
+                item.publishedAt
+            }</div>
+                </div>
+            </div>`;
+        })
+        .join("");
+    console.log("newsHTML", newsHTML); // newsList는 배열이기 때문에 ','까지 출력이 된다. join()을 사용하여 배열을 string타입으로 변환하여 ''을 넣어 ,를 삭제하자
+    document.getElementById("newsBoard").innerHTML = newsHTML;
 };
 
 getNews();
