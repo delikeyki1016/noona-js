39a50e307a27ce09dd103589eff7254a11811c9e
delikeyki1016 <149133856+delikeyki1016@users.noreply.github.com>
2024-07-10 23:42:29 +0900

    7/10 과제 제출

--- 0708_news/news.js
+++ 0708_news/news.js
@@ -15,10 +15,15 @@
 const API_KEY = `70a9dc1efaba49299c95e70ba34ae4ab`;
 
 let newsList = [];
+let category = "";
 
 const getNews = async () => {
     const url = new URL(
-        `https://beautiful-torte-d702c9.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`
+        `${
+            category && category !== ""
+                ? `https://newsapi.org/v2/top-headlines/sources?category=${category}&apiKey=${API_KEY}`
+                : `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
+        } `
     );
     console.log("url:", url);
 
@@ -26,7 +31,13 @@ const getNews = async () => {
     console.log("rsponse:", response);
     const data = await response.json(); // json파일형태로 data변수에 선언
     console.log("data:", data);
-    newsList = data.articles;
+    newsList = [];
+    console.log("다시배열내용", newsList);
+    if (Array.isArray(data.sources)) {
+        newsList = data.sources;
+    } else if (data.articles) {
+        newsList = data.articles;
+    }
     console.log("newsList:", newsList);
     render();
 };
@@ -36,7 +47,8 @@ const render = () => {
 
     newsHTML = newsList
         .map((item) => {
-            return `<div class="row p-2 border-bottom">
+            if (!item.id) {
+                return `<div class="row p-2 border-bottom">
                 <div class="col-lg-4">
                 ${
                     item.urlToImage && item.urlToImage
@@ -46,16 +58,94 @@ const render = () => {
                 </div>
                 <div class="col-lg-8 d-flex flex-column">
                     <h2>${item.title}</h2>
-                    <p>${item.description}</p>
-                    <div class="mt-auto">${item.author} | ${
-                item.publishedAt
-            }</div>
+                    <p>${
+                        item.description === null || item.description === ""
+                            ? "내용없음"
+                            : truncateText(item.description)
+                    }</p>
+                    <div class="mt-auto">${
+                        item.author === null ? "no source" : item.author
+                    } | 
+                    ${moment(item.publishedAt).startOf("hour").fromNow()}</div>
                 </div>
             </div>`;
+            } else {
+                return `<div class="row p-2 border-bottom">
+                <div class="col-lg-12 d-flex flex-column">
+                    <h2>${item.name}</h2>
+                    <p>${
+                        item.description === null || item.description === ""
+                            ? "내용없음"
+                            : truncateText(item.description)
+                    }</p>
+                    <div class="mt-auto">${
+                        item.id === null ? "no source" : item.id
+                    } | 
+                    ${item.url === null ? "no url" : item.url}</div>
+                </div>
+            </div>`;
+            }
         })
         .join("");
-    console.log("newsHTML", newsHTML); // newsList는 배열이기 때문에 ','까지 출력이 된다. join()을 사용하여 배열을 string타입으로 변환하여 ''을 넣어 ,를 삭제하자
+    // console.log("newsHTML", newsHTML); // newsList는 배열이기 때문에 ','까지 출력이 된다. join()을 사용하여 배열을 string타입으로 변환하여 ''을 넣어 ,를 삭제하자
     document.getElementById("newsBoard").innerHTML = newsHTML;
 };
 
 getNews();
+
+// UI
+const searchForm = document.getElementById("searchBox");
+const showInputBox = () => {
+    searchForm.style.display === "none"
+        ? (searchForm.style.display = "block")
+        : (searchForm.style.display = "none");
+};
+
+const navBar = document.getElementById("navBar");
+const showMenu = () => {
+    navBar.style.display = "block";
+    document.querySelector("html").style.overflow = "hidden";
+};
+
+const navBarClose = () => {
+    navBar.style.display = "none";
+    document.querySelector("html").style.overflow = "auto";
+};
+
+// 문자열 자르기 200자까지 되는 것이 별로 없어보여서 100자로 자름
+function truncateText(text) {
+    return text.length > 101 ? text.substring(0, 100) + "..." : text;
+}
+
+// 카테고리별 뉴스 검색
+const categoryAll = document.querySelectorAll(".list-category > li > a"); //노드리스트임
+console.log("카테고리", categoryAll);
+const categoryArr = Array.from(categoryAll); // map함수를 사용하기 위해 배열로 변환
+console.log("카테고리 배열", categoryArr);
+
+// 모바일에서 카테고리를 클릭하면 해당 레이어가 닫히게 처리
+let maxScreenWidth = 576; //체크할 최대 가로 사이즈
+function isBrowserWidthBelow(maxWidth) {
+    var currentWidth = window.innerWidth; //현재 브라우저의 가로 사이즈 가져오기
+    return currentWidth <= maxWidth; //기준사이즈보다 작거나같으면 true 반환
+}
+
+categoryArr.map((cate) => {
+    cate.addEventListener("click", (event) => {
+        category = event.target.textContent;
+        console.log("변경된 카테고리", category);
+        if (isBrowserWidthBelow(maxScreenWidth)) {
+            // 모바일에서는 카테고리 클릭 시 카테고리영역 닫기
+            navBar.style.display = "none";
+            document.querySelector("html").style.overflow = "auto";
+        }
+        getNews();
+    });
+});
+
+// 로고를 누르면 첫로딩때 처럼 topheadline으로 랜더링되기
+const btnHome = document.getElementById("btn-home");
+btnHome.addEventListener("click", function () {
+    category = "";
+    getNews();
+});
